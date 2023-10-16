fba383eec42bc568607fe5fc3bd0dcfd42f7a16d
Ujwal-Karthik <ujwalkms@gmail.com>
2023-10-16 11:49:34 +0530

    feat: Added Sorting

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Item } from "../types/types";
 import ListItem from "./ListItem";
 
@@ -8,10 +9,27 @@ interface itemsList {
 }
 
 function List({ items, onDeleteItem, onToggleCheckBox }: itemsList) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems: Item[] = [];
+
+  if (sortBy === "input") {
+    sortedItems = items;
+  }
+  if (sortBy === "alphabetically") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.itemName.localeCompare(b.itemName));
+  }
+  if (sortBy === "packingStatus") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(b.packed) - Number(a.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item: Item) => (
+        {sortedItems.map((item: Item) => (
           <ListItem
             item={item}
             key={item.id}
@@ -20,6 +38,26 @@ function List({ items, onDeleteItem, onToggleCheckBox }: itemsList) {
           ></ListItem>
         ))}
       </ul>
+      <div className="actions">
+        <select
+          value={sortBy}
+          onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
+            setSortBy(event.target.value);
+          }}
+        >
+          <option value={"input"}>Sort by Input order</option>
+          <option value={"alphabetically"}>Sort Alphabetically</option>
+          <option value={"packingStatus"}>Sort by Packed Status</option>
+        </select>
+        <button
+          onClick={() => {
+            console.log("HEY");
+            sortedItems.length = 0;
+          }}
+        >
+          Clear List
+        </button>
+      </div>
     </div>
   );
 }
